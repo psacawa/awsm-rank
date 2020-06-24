70ae3c779e5b0b076dbdb23de865e50934a9e774
Paweł Sacawa <psacawa@math.toronto.edu>
2020-06-23 20:17:31 -0400

    Add: --open option for opening urls in firefox...
    This randomly decides to open a single new window or many...

--- awsm-rank.py
+++ awsm-rank.py
@@ -7,6 +7,7 @@ import json
 from typing import List
 from pprint import pprint
 import logging
+import subprocess
 
 from aiohttp import ClientSession, ClientError
 import requests
@@ -14,8 +15,9 @@ from bs4 import BeautifulSoup
 from tabulate import tabulate
 
 logger = logging.getLogger()
-logger.setLevel (logging.INFO)
-logger.addHandler (logging.StreamHandler (sys.stdout))
+logger.setLevel(logging.INFO)
+logger.addHandler(logging.StreamHandler(sys.stdout))
+
 
 def main():
     """Scrape and parse a github page. For all linked github projects, determine the number
@@ -24,21 +26,36 @@ def main():
     parser.add_argument("url", type=str)
     parser.add_argument("--token", type=str)
     parser.add_argument("--limit", type=int)
-    parser.add_argument("--debug", dest='debug', action='store_true')
+    parser.add_argument("--debug", dest="debug", action="store_true")
+    parser.add_argument("--open", dest="open", action="store_true")
     args = parser.parse_args()
     if args.debug:
-        logger.setLevel (logging.DEBUG)
+        logger.setLevel(logging.DEBUG)
 
     projects = get_linked_projects(args.url)
     repo_endpoints = get_repo_api_endpoints(projects)
     ranking = asyncio.run(get_stargazer_counts(repo_endpoints, token=args.token))
     if args.limit:
-        ranking = ranking[:limit]
-    print_ranking(ranking)
+        ranking = ranking[: args.limit]
+    if args.open:
+        open_urls(ranking)
+    else:
+        print_ranking(ranking)
 
 
 def print_ranking(ranking):
+    #  don't display urls
+    for item in ranking:
+        item.pop("url")
     print(tabulate(ranking, headers="keys"))
+
+
+def open_urls(ranking):
+    """Open the urls in the ranking in firefox. Add option to choose the web browser?"""
+    urls = [item["url"] for item in ranking]
+    firefox_cmd = "firefox".split() + urls
+    logger.debug(f"Executing: {' '.join (firefox_cmd)}")
+    subprocess.run(firefox_cmd)
 
 
 def get_linked_projects(url):
@@ -60,7 +77,7 @@ def get_repo_api_endpoints(projects: List[re.Match]):
     return [
         f"https://api.github.com/repos/{group['user']}/{group['repo']}"
         for group in groups
-        if group["user"] not in["apps", "site"]
+        if group["user"] not in ["apps", "site"]
     ]
 
 
@@ -76,6 +93,7 @@ async def get_ranking_data(session: ClientSession, repo_url):
                 "name": data["name"],
                 "owner": data["owner"]["login"],
                 "stargazers": data["stargazers_count"],
+                "url": data["html_url"],
             }
     except KeyError as e:
         logger.error(f"Response malformed at {repo_url}- authentication failed?")
@@ -89,7 +107,7 @@ async def get_stargazer_counts(repos, token=None):
         logger.debug("beginning session")
         tasks = [get_ranking_data(session, repo) for repo in repos]
         ranked_repos = await asyncio.gather(*tasks)
-        logger.debug (ranked_repos)
+        logger.debug(ranked_repos)
     ranked_repos = sorted(ranked_repos, key=lambda x: x["stargazers"], reverse=True)
     return ranked_repos
 
