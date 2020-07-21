720662eb6158b81f4a484578f71e3dca7aac9e03
Paweł Sacawa <psacawa@math.toronto.edu>
2020-07-20 20:10:40 -0400

    Add: Description & author

--- setup.py
+++ setup.py
@@ -3,7 +3,10 @@ from setuptools import setup, find_packages
 
 setup(
     name="awsm-ranker",
-    version=read('VERSION').strip('\n')
+    version=read('VERSION').strip('\n'),
+    description="Ranks github repo entries on github pages from the 'Awesome' series",
+    author="Paweł Sacawa",
+    url='https://github.com/psacawa/awsm-ranker',
     packages=find_packages(),
     entry_points={
         "console_scripts": ["awsm-rank = awsm_ranker.awsm_rank:main"]
